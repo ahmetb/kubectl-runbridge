584027b214d590c0704b7d7511f9d5ea725494e8
Ahmet Alp Balkan <ahmetb@google.com>
2021-01-20 12:12:39 -0800

    implement basic reverse proxying
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- README.md
+++ README.md
@@ -2,4 +2,31 @@
 
 -----
 
+Examples:
+
+```sh
+$ kubectl api-versions
+domains.cloudrun.com/v1alpha1
+serving.knative.dev/v1
+```
+
+```sh
+$ kubectl api-resources
+NAME             SHORTNAMES      APIVERSION                      NAMESPACED   KIND
+domainmappings                   domains.cloudrun.com/v1alpha1   true         DomainMapping
+configurations   config,cfg      serving.knative.dev/v1          true         Configuration
+revisions        rev             serving.knative.dev/v1          true         Revision
+routes           rt              serving.knative.dev/v1          true         Route
+services         kservice,ksvc   serving.knative.dev/v1          true         Service
+```
+
+```
+$ kubectl get ksvc
+kubectl get ksvc
+NAME                       AGE
+object-detection           35d
+chat                       14d
+whiteboard                 14d
+```
+
 See [LICENSE].
--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 var (
+	// TODO(ahmetb) bundle these into the binary
 	discoveryDocs = map[string]string{
 		"":                               "discovery/apis.json",
 		"/serving.knative.dev/v1":        "discovery/api-serving.json",
@@ -18,22 +22,32 @@ var (
 )
 
 func main() {
-	http.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `{
-			"kind": "APIVersions",
-			"versions": [
-			  "v1"
-			]
-		}`)
-	})
-	http.HandleFunc("/apis", discovery)
-	http.HandleFunc("/apis/", discovery)
+	r := mux.NewRouter()
+	r.HandleFunc("/{region}/api/v1", baseAPIv1).Methods(http.MethodGet, http.MethodHead)
+	r.HandleFunc("/{region}/apis", discovery).Methods(http.MethodGet, http.MethodHead)
+	r.HandleFunc("/{region}/apis/{apiGroup}/{apiVersion}", discovery).Methods(http.MethodGet, http.MethodHead)
+	r.HandleFunc("/{region}/apis/{apiGroup}/{apiVersion}/namespaces/{ns}/{resource:.*}", reverseProxy)
 	fmt.Println("starting polyfill kube-apiserver for Cloud Run")
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:5555", nil))
 }
 
-func discovery(w http.ResponseWriter, r *http.Request) {
+func baseAPIv1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `{
+		"kind": "APIVersions",
+		"versions": [
+		  "v1"
+		]
+	}`)
+}
+
+func pathWithoutRegionPrefix(r *http.Request) string {
 	path := r.URL.Path
+	return strings.TrimPrefix(path, "/"+mux.Vars(r)["region"])
+}
+
+func discovery(w http.ResponseWriter, r *http.Request) {
+	path := pathWithoutRegionPrefix(r)
 	path = strings.TrimPrefix(path, "/apis")
 
 	if path == "/" {
@@ -46,6 +60,7 @@ func discovery(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "api %q not handled", path)
 		return
 	}
+	// TODO(ahmetb) implement caching if necessary
 	f, err := os.Open(fp)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -54,4 +69,55 @@ func discovery(w http.ResponseWriter, r *http.Request) {
 	}
 	defer f.Close()
 	io.Copy(w, f)
+}
+
+func reverseProxy(w http.ResponseWriter, r *http.Request) {
+	path := pathWithoutRegionPrefix(r) // e.g. /apis/serving.knative.dev/v1/namespaces/ahmetb-demo/services/foo
+	region := mux.Vars(r)["region"]
+
+	fmt.Println(region)
+	fmt.Println(path)
+
+	tok, err := getAccessToken()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "failed to get access_token: %v", err)
+		return
+	}
+
+	endpoint := fmt.Sprintf("%s-run.googleapis.com", region)
+	r.URL.Host = endpoint
+	r.URL.Scheme = "https"
+	r.URL.Path = path
+	r.RequestURI = ""
+	r.Host = endpoint
+	r.RemoteAddr = ""
+	r.Header.Add("authorization", "Bearer "+tok)
+	r.Header.Set("host",endpoint)
+
+	fmt.Println(r.URL)
+	// TODO(ahmetb) implement this as a proper http reverse proxy
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "reverse proxy error: %v", err)
+		return
+	}
+	for k, vs := range resp.Header {
+		for _, v := range vs {
+			w.Header().Add(k, v)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+
+	if resp.Body != nil {
+		io.Copy(w, resp.Body)
+		resp.Body.Close()
+	}
+}
+
+func getAccessToken() (string, error) {
+	cmd := exec.Command("gcloud", "auth", "print-access-token", "-q")
+	b, err := cmd.Output()
+	return strings.TrimSuffix(string(b), "\n"), err
 }
